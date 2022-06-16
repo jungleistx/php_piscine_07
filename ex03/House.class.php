a304ca9b43869706b89619272f5ff1bd3fd197f3
Roope Vuorenlehto <rvuorenl@c2r4p12.hive.fi>
2022-06-16 17:30:08 +0300

    ex03, forgot abstract

--- ex03/House.class.php
+++ ex03/House.class.php
@@ -1,13 +1,12 @@
 <?php
-class House {
+abstract class House {
 
 	abstract function getHouseName();
 	abstract function getHouseSeat();
 	abstract function getHouseMotto();
 
 	public function introduce() {
-		print 'House ' . $this->getHouseName() . ' of ' . $this->getHouseSeat() .
-		' : "' . $this->getHouseMotto() . '"' . PHP_EOL;
+		print 'House ' . $this->getHouseName() . ' of ' . $this->getHouseSeat() . ' : "' . $this->getHouseMotto() . '"' . PHP_EOL;
 	}
 }
 ?>
